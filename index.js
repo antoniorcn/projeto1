fe46c4483a89c498d525e38c60d9e90bdf788d42
antoniorcn <antoniorcn@hotmail.com>
2022-04-18 09:36:31 -0300

    Update index.js
    colocando dependencias do PG

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const pg = require('pg');
 const port = 80;
 const app = express();
+
+const pool = new pg.Pool(
+    {
+        connectionString: 
+        "postgres://fivrtpgwuangyi:c81fab4e020e53629101c422750d6d7e881a1b33a130d28df2a3f437eaeb2303@ec2-3-233-43-103.compute-1.amazonaws.com:5432/d1mjgmpk8o2peb",
+        ssl: { 
+            rejectUnauthorized: false
+        }
+    }
+);
 
 app.set('port', port);
 
